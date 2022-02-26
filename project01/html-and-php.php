1aea8406d54faeded3955b7de30c0c6b4bbe980d
Nia Waromi <91767702+Nianaaaa@users.noreply.github.com>
2022-02-26 08:50:46 +0700

    commit project 01

--- project01/html-and-php.php
+++ project01/html-and-php.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profile - SAGU Ekskul Day 02</title>
+    <title>HTML and PHP</title>
 </head>
 <body>
-    <h1>Aftania Waromi</h1>
+    <?php print ("Hello SaCode!") ?>
 </body>
 </html>
---
+++ project01/php-and-html.php
@@ -0,0 +1,16 @@
+<?php 
+ print("
+  <html>
+    <head>
+      <title>PHP and HTML</title>
+    </head>
+
+    <body>
+      <h1>Hello SAGU</h1>
+      <p> We are learning PHP Codes. </p>
+    </body>
+    
+  </html>
+ 
+ ");
+?>
